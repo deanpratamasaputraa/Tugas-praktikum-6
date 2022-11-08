4cafe31de909b26ad1a97390bf52211b3d6c54cf
syifaaurelia <syifarrahma2@gmail.com>
2022-11-08 10:24:39 +0700

    masuk

---
+++ Lab2StrukturKondisi/Latihan1.py
@@ -0,0 +1,9 @@
+# menentukan bilangan terbesar dari dua input
+print("------script Syifa------")
+print("Menentukan bilangan terbesar dari dua bilangan")
+a: int = int(input("Masukkan nilai A : "))
+b = int(input("Masukkan nilai B : "))
+if a > b:
+    print("A=", a, "yang terbesar")
+elif b > a:
+    print("B=", b, "yang terbesar")
---
+++ Lab3Perulangan/Latihan2.py
@@ -0,0 +1,8 @@
+from random import random
+
+n = int(input("Masukkan jumlah n 5 : "))
+while n==n:
+    break
+for i in range(n):
+    bil = random()%0.5
+    print("Perulangan ke : ", bil)
